12b8f65dcde549e379df73c44ce65baa04dbd739
YangZhi1 <lyangzhi1@gmail.com>
2020-10-10 10:13:07 +0800

    No changes made

--- Production/app.py
+++ Production/app.py
@@ -17,8 +17,13 @@ except Exception as e:
 @app.route('/')
 def index():
     # TODO: To define how we are going limit the entries
-    metadata = database.find().limit(10)
-    return render_template('index.html', books=metadata)
+    books_list = database.find().limit(10)
+    return render_template('index.html', books=books_list, query=None)
+
+@app.route('/book/<asid>')
+def book(asid):
+    # TODO: To define how we are going limit the entries
+    return render_template('index.html')    
 
 @app.route('/add', methods=['POST'])
 def add_book():
@@ -26,12 +31,19 @@ def add_book():
     # database.insert_one({'text' : new_todo, 'complete' : False})
     return redirect(url_for('index'))
 
-@app.route('/complete/<oid>')
-def complete(oid):
-    # todo_item = metadata.find_one({'_id': ObjectId(oid)})
-    # todo_item['complete'] = True
+@app.route('/search', methods=['POST'])
+def search():
+    # 'asin': 'B000FA5S98'
+    # search_item = database.find_one({'_id': ObjectId(oid)})
     # database.save(todo_item)
-    return redirect(url_for('index'))
+    search_input = request.form['search-book']
+    return redirect(url_for('results', query = search_input))
+
+@app.route('/search/<query>')
+def results(query):
+    search_results = database.find({'asin': {"$regex": query , "$options": "i"}}).limit(10)
+    print(query)
+    return render_template("index.html", books=search_results, query=query)
 
 @app.route('/delete_completed')
 def delete_completed():
@@ -43,7 +55,7 @@ def delete_all():
     # database.delete_many({})
     return redirect(url_for('index'))
 
-@app.route('/not_found', methods=['GET', 'POST'])
+@app.route('/404', methods=['GET', 'POST'])
 def not_found():
     if request.method == 'POST':
         return redirect(url_for('index'))
