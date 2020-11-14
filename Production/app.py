84ade8f2967304dc452d7bb2a7fa9efb4ebb19e3
x_ming <xianming@infeniontech.com>
2020-11-14 18:32:11 +0800

    Log search and viewing history

--- Production/app.py
+++ Production/app.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from flask_paginate import Pagination, get_page_parameter
 from bson.objectid import ObjectId
 from functools import wraps
-import time
+import datetime
 
 #import mysqlpython
 
@@ -15,8 +15,9 @@ app.secret_key = "secretkey"
 
 try:
     mongo = PyMongo(app)
-    database = mongo.db["metadata"]
+    metadata_db = mongo.db["metadata"]
     user_db = mongo.db["user"]
+    logs_db = mongo.db["logs"]
 
     # UNCOMMENT WHEN MySQL IS UP
     # mysql_db = mysqlpython.mysql_review()
@@ -30,21 +31,24 @@ import routes
 @app.route('/')
 def index():
     # TODO: To define how we are going limit the entries
-    books_list = database.find().limit(4)
+    books_list = metadata_db.find().limit(4)
     return render_template('index.html', books=books_list, query=None, pagination=None)
 
 @app.route('/book/<asin>')
 def book(asin):
-    book = database.find_one({'asin': asin})
+    book = metadata_db.find_one({'asin': asin})
     # UNCOMMENT WHEN MySQL IS UP
     #review_text = mysql_db.get_review_by_asin(asin)
+    if 'logged-in' in session:
+        logs_db.insert_one({"user": session['user']['email'], "action":"view", "content": book['title'], "datetime": datetime.datetime.now()})
+        print("Logged")
     return render_template('book.html', book=book) # add review_text into render_template
 
 @app.route('/book/add_review/<asin>')
 #@login_required
 def add_review(asin):
-    book = database.find_one({'asin': asin})
-    #TODO: add this review into the database
+    book = metadata_db.find_one({'asin': asin})
+    #TODO: add this review into the metadata_db
 
     # retrieve new user review from the html input
     
@@ -77,22 +81,25 @@ def add_review(asin):
 @app.route('/add', methods=['POST'])
 def add_book():
     # new_todo = request.form.get('new-todo')
-    # database.insert_one({'text' : new_todo, 'complete' : False})
+    # metadata_db.insert_one({'text' : new_todo, 'complete' : False})
     return redirect(url_for('index'))
 
 @app.route('/search', methods=['POST'])
 def search():
     # 'asin': 'B000FA5S98'
-    # search_item = database.find_one({'_id': ObjectId(oid)})
-    # database.save(todo_item)
+    # search_item = metadata_db.find_one({'_id': ObjectId(oid)})
+    # metadata_db.save(todo_item)
     search_input = request.form['search-book']
+    if 'logged-in' in session:
+        logs_db.insert_one({"user": session['user']['email'], "action":"search", "content": search_input, "datetime": datetime.datetime.now()})
+        print("Logged")
     return redirect(url_for('results', query = search_input))
 
 @app.route('/search/<query>')
 def results(query):
     per_page = 12
     page = request.args.get(get_page_parameter(), type=int, default=1)
-    search_results = database.find({'$or': [{'title': {"$regex": query , "$options": "i"}}, {'author': {"$regex": query , "$options": "i"}}]}).skip((page - 1) * per_page).limit(per_page)
+    search_results = metadata_db.find({'$or': [{'title': {"$regex": query , "$options": "i"}}, {'author': {"$regex": query , "$options": "i"}}]}).skip((page - 1) * per_page).limit(per_page)
     pagination = Pagination(page=page, per_page=per_page ,total=search_results.count(), search=False, record_name='search_results')
     return render_template("index.html", books=search_results, query=query, pagination=pagination)
 
@@ -100,7 +107,7 @@ def results(query):
 def category(query):
     per_page = 12
     page = request.args.get(get_page_parameter(), type=int, default=1)
-    search_results = database.find({'genre': {"$regex": query , "$options": "i"}}).skip((page - 1) * per_page).limit(per_page)
+    search_results = metadata_db.find({'genre': {"$regex": query , "$options": "i"}}).skip((page - 1) * per_page).limit(per_page)
     print(search_results)
     pagination = Pagination(page=page, per_page=per_page ,total=search_results.count(), search=False, record_name='search_results')
     return render_template("index.html", books=search_results, query=query, pagination=pagination)
