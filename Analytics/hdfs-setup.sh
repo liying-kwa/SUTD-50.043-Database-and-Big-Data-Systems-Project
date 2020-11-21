7aeb8a2115c546095845a290d706c0aefd5f95b6
Li Ying Kwa <li5ying@hotmail.com>
2020-11-21 18:44:45 +0800

    hdfs-setup part 2: setup user hadoop with superuser privileges without password

--- Analytics/hdfs-setup.sh
+++ Analytics/hdfs-setup.sh
@@ -6,7 +6,7 @@ export NAMENODE_IP=`cat namenode_ip.txt`
 export DATANODE_IP_ARR=(`cat datanode_ip.txt | tr "\n" " "`)
 export n=${#DATANODE_IP_ARR[@]}
 
-# Setup hostnames by appending to /etc/hosts for all nodes
+# Write hosts.txt file for editing /etc/hosts
 cd ./hdfs-setup
 touch hosts.txt
 echo -e "${NAMENODE_IP}\tcom.analytics.namenode" >> ./hosts.txt
@@ -14,13 +14,19 @@ for i in "${!DATANODE_IP_ARR[@]}"
 do
 	echo -e "${DATANODE_IP_ARR[$i]}\tcom.analytics.datanode$((i + 1))" >> ./hosts.txt
 done
-# For name node
+
+
+# Setup for name node part 1
 scp -i ../kp.pem -o StrictHostKeyChecking=no ./namenode-setup.sh ./hosts.txt ubuntu@${NAMENODE_IP}:~/
 echo -e "bash ./namenode-setup.sh" | ssh -i ../kp.pem -o "StrictHostKeyChecking no" ubuntu@${NAMENODE_IP}
-# For data nodes
+
+# Setup for data nodes part 1
 for DATANODE_IP in "${DATANODE_IP_ARR[@]}"
 do
     scp -i ../kp.pem -o StrictHostKeyChecking=no ./datanode-setup.sh ./hosts.txt ubuntu@${DATANODE_IP}:~/
     echo -e "bash ./datanode-setup.sh" | ssh -i ../kp.pem -o "StrictHostKeyChecking no" ubuntu@${DATANODE_IP}
 done
+
+
+
 
--- Analytics/hdfs-setup/datanode-setup.sh
+++ Analytics/hdfs-setup/datanode-setup.sh
@@ -4,3 +4,7 @@
 # Setup the hostname
 sudo sh -c 'echo -e "\n" >> /etc/hosts'
 sudo sh -c 'cat ./hosts.txt >> /etc/hosts'
+
+# Create user hadoop and grant superuser privileges
+sudo adduser --system --shell /bin/bash --gecos 'User for managing of hadoop' --group --disabled-password --home /home/hadoop hadoop
+sudo sh -c 'echo "hadoop ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-hadoop'
