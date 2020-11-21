f13989e39d99534845aa4e5941b07bc9e4a59cf4
Li Ying Kwa <li5ying@hotmail.com>
2020-11-21 18:03:03 +0800

    hdfs-setup part 1-2

--- Analytics/hdfs-setup.sh
+++ Analytics/hdfs-setup.sh
@@ -6,7 +6,7 @@ export NAMENODE_IP=`cat namenode_ip.txt`
 export DATANODE_IP_ARR=(`cat datanode_ip.txt | tr "\n" " "`)
 export n=${#DATANODE_IP_ARR[@]}
 
-# Setup hostname
+# Setup hostnames by appending to /etc/hosts for all nodes
 cd ./hdfs-setup
 touch hosts.txt
 echo -e "${NAMENODE_IP}\tcom.analytics.namenode" >> ./hosts.txt
@@ -14,6 +14,13 @@ for i in "${!DATANODE_IP_ARR[@]}"
 do
 	echo -e "${DATANODE_IP_ARR[$i]}\tcom.analytics.datanode$((i + 1))" >> ./hosts.txt
 done
+# For name node
 scp -i ../kp.pem -o StrictHostKeyChecking=no ./namenode-setup.sh ./hosts.txt ubuntu@${NAMENODE_IP}:~/
 echo -e "bash ./namenode-setup.sh" | ssh -i ../kp.pem -o "StrictHostKeyChecking no" ubuntu@${NAMENODE_IP}
+# For data nodes
+for DATANODE_IP in "${DATANODE_IP_ARR[@]}"
+do
+    scp -i ../kp.pem -o StrictHostKeyChecking=no ./datanode-setup.sh ./hosts.txt ubuntu@${DATANODE_IP}:~/
+    echo -e "bash ./datanode-setup.sh" | ssh -i ../kp.pem -o "StrictHostKeyChecking no" ubuntu@${DATANODE_IP}
+done
 
