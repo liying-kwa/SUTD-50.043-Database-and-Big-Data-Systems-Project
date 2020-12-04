7809713dd217217f3670496d087baf23d68b34c1
Unknown <tiangpeiyuan@yahoo.com.sg>
2020-12-05 00:04:09 +0800

    pymongotest split delimiter by "\t"

--- Analytics/ingestion/namenode-ingestion.sh
+++ Analytics/ingestion/namenode-ingestion.sh
@@ -5,7 +5,7 @@ export mysql_ip=18.140.53.151
 
 # TODO CHECK INGESTION OF DATA and somehow edit mysql_ip
 #/opt/sqoop-1.4.7/bin/sqoop import-all-tables --connect jdbc:mysql://${mysql_ip}/mydb?useSSL=false --username userall --password password
-/opt/sqoop-1.4.7/bin/sqoop import-all-tables --connect jdbc:mysql://ec2-18-141-56-18.ap-southeast-1.compute.amazonaws.com/mydb?useSSL=false --username sqoop --password sqoop123
+/opt/sqoop-1.4.7/bin/sqoop import-all-tables --connect jdbc:mysql://ec2-122-248-220-104.ap-southeast-1.compute.amazonaws.com/mydb?useSSL=false --username userall2 --password password2 --fields-terminated-by "\t"
 
 # TEST PYMONGO
-python3 /home/ubuntu/pymongotest.py ec2-52-221-250-84.ap-southeast-1.compute.amazonaws.com 1603420304
+python3 /home/ubuntu/pymongotest.py ec2-13-212-212-123.ap-southeast-1.compute.amazonaws.com 1603420304
