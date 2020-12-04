fdea11c060f894b9cd2e484210964f2ac13d9b3d
Li Ying Kwa <li5ying@hotmail.com>
2020-12-04 13:16:35 +0800

    sleep longer

--- Analytics/hdfs-setup/part6-namenode-setup.sh
+++ Analytics/hdfs-setup/part6-namenode-setup.sh
@@ -12,6 +12,6 @@ sudo chown -R hadoop:hadoop /mnt/hadoop/namenode
 # Format hdfs
 echo 'Y' | /opt/hadoop-3.3.0/bin/hdfs namenode -format
 
-# Sleep for a while for datanode port to bind properly later
-echo "Sleeping for 10 seconds..."
-sleep 10
+# Sleep for a while for datanode to start properly later
+echo "Sleeping for 20 seconds..."
+sleep 20
