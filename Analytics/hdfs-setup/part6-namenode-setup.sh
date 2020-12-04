34531c720177199e1d2e8fda0a027d1fc8054ebe
Li Ying Kwa <li5ying@hotmail.com>
2020-12-04 10:57:44 +0800

    fixing datanode binding to port problem

--- Analytics/hdfs-setup/part6-namenode-setup.sh
+++ Analytics/hdfs-setup/part6-namenode-setup.sh
@@ -11,3 +11,7 @@ sudo chown -R hadoop:hadoop /mnt/hadoop/namenode
 
 # Format hdfs
 echo 'Y' | /opt/hadoop-3.3.0/bin/hdfs namenode -format
+
+# Sleep for a while for datanode port to bind properly later
+echo "Sleeping for 10 seconds..."
+sleep 10
