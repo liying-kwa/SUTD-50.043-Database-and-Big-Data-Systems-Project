34258ef4837bfc1b06b9906fa3a148b4f08c1ab5
Li Ying Kwa <li5ying@hotmail.com>
2020-12-06 02:23:23 +0800

    final analytics run #4: changed sqoop sql creds back

--- Analytics/ingestion/namenode-ingestion.sh
+++ Analytics/ingestion/namenode-ingestion.sh
@@ -10,7 +10,7 @@ export SQL_HOSTNAME=`cat /home/hadoop/sql-hostname.txt`
 export MONGODB_HOSTNAME=`cat /home/hadoop/mongodb-hostname.txt`
 
 # Ingest data from MySQL into HDFS
-/opt/sqoop-1.4.7/bin/sqoop import-all-tables --connect jdbc:mysql://${SQL_HOSTNAME}/mydb?useSSL=false --username useralldb20 --password passworddb20 --fields-terminated-by "\t" --as-parquetfile -m1
+/opt/sqoop-1.4.7/bin/sqoop import-all-tables --connect jdbc:mysql://${SQL_HOSTNAME}/mydb?useSSL=false --username sqoop --password sqoop123 --fields-terminated-by "\t" --as-parquetfile -m1
 
 # Ingest data from MongoDB into HDFS
 python3 ./ingestmongo.py ${MONGODB_HOSTNAME}
