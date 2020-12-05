fdf6f1a2aa41c7342845b743517808d3c546ad86
Unknown <tiangpeiyuan@yahoo.com.sg>
2020-12-05 14:06:08 +0800

    brute forcing, python not found. mongodb also not working.

--- Analytics/ingestion/pymongotest.py
+++ Analytics/ingestion/pymongotest.py
@@ -32,16 +32,63 @@ for post in db["new_kindle_metadata"].find({"asin" : asin}):
     pprint.pprint(post)
     print(post["price"]) # This gives 7.69, type "str".
 
+price_list = []
+asin_list = []
+# Adding all the prices and asin to 2 lists to make RDD later
+for post in db["new_kindle_metadata"].find():
+    if "price" in post:
+        price_list.append(post["price"])
+    else:
+        price_list.append("NP")
+    asin_list.append(post["asin"])
+
+print(price_list[:30])
+print(asin_list[:30])
+
+
+
 sc = pyspark.SparkContext("spark://172.2.0.147:7077", "Correlation")
-spark = SparkSession(sc)
+spark = SparkSession(sc).builder.master("172.2.0.147").appName("HELLO") \
+         .config("spark.mongodb.input.uri", "mongodb://user:password@13.212.212.123:27017") \
+            .config('spark.driver.extraClassPath', '/opt/spark-3.0.1-bin-hadoop3.2/jars/*;/opt/spark-3.0.1-bin-hadoop3.2/bin/pyspark') \
+               .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.11:2.4.2') \
+                  .getOrCreate()
 
-review = spark.read.parquet("hdfs://172.2.0.147:9000/user/hadoop/KRTable/9d5d6615-68be-427e-b214-b3be7549880e.parquet")
 
 
 
 
-#asindb = reviews1.select('asin','reviewText')
-#asindb_len = asindb.withColumn('reviewLength', fns.length('reviewText'))
+
+# Parquet stuff
+reviews = spark.read.parquet("hdfs://172.2.0.147:9000/user/hadoop/KRTable/9d5d6615-68be-427e-b214-b3be7549880e.parquet")
+#DataFrame[id: int, asin: string, helpful: string, overall: int, reviewText: string, reviewTime: string, reviewerID: string, reviewerNam$
+#reviews.show(5)
+
+
+# spark mongo if got time
+
+#meta = spark.read.format("mongo").option("uri", "mongodb://user:password@13.212.212.123:27017/user.metadatas?authSource=user").load()
+#meta.printSchema()
+
+#df = spark.read.format("com.mongodb.spark.sql.DefaultSource").load()
+#df.printSchema()
+
+price_rdd = spark.sparkContext.parallelize(price_list)
+asin_rdd = spark.sparkContext.parallelize(asin_list)
+
+print(price_rdd.take(5))
+print(asin_rdd.take(5))
+
+
+
+asindb = reviews.select('asin','reviewText')
+
+asindb_len = asindb.withColumn('reviewLength', fns.length('reviewText'))
+#asindb_len.show(5)
+
+# Group tgt average length by asin
+asin_avglen = asindb_len.groupBy('asin').avg('reviewLength')
+asin_avglen.show(5)
 
 
 
