aa48ab02d94bd728f582a37e03858132be3031fd
Li Ying Kwa <li5ying@hotmail.com>
2020-12-06 03:17:40 +0800

    final analytics run #6: wrong filename, should be namenode-calculation.sh not .py

--- Analytics/calculation.sh
+++ Analytics/calculation.sh
@@ -11,7 +11,7 @@ export DATANODE_IP_ARR_PRIV=(`cat datanode_ip_priv.txt | tr "\n" " "`)
 # Move to the right folder and calculate
 cd ./calculation/
 scp -i ../kp.pem ./namenode-calculation.sh ./pearson.py ./tfidf.py ubuntu@${NAMENODE_IP}:~/
-ssh -i ../kp.pem ubuntu@${NAMENODE_IP} "sudo -u hadoop sh -c 'bash ./namenode-calculation.py'"
+ssh -i ../kp.pem ubuntu@${NAMENODE_IP} "sudo -u hadoop sh -c 'bash ./namenode-calculation.sh'"
 
 
 echo "END OF CALCULATION"
